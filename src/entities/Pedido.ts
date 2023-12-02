bc4217d42f03c851e5a68c658eb3246abf77a7dc
Camila Moura Rodrigues de Miranda <cmiranda@fastprobr.com>
2023-12-01 22:32:09 -0300

    feat: clear architecture

--- src/entities/Pedido.ts
+++ src/entities/Pedido.ts
@@ -1,4 +1,4 @@
-import { Cliente } from "./cliente";
+import { Cliente } from "./Cliente";
 import ProdutosDoPedido from "@/old/adapters/Driver/ProdutosDoPedido";
 import { Pagamento } from "@prisma/client";
 

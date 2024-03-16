eb2602b7a11d4cbf2a7b278f223354351d27870f
pandaluk <arthur.silva1@hotmail.com.br>
2024-03-15 23:03:00 -0300

    #refactor: Remove presenter não utilizado

--- src/interfaces/presenters/IPedidoPresenter.ts
+++
@@ -1,6 +0,0 @@
-import { Pedido } from "@/entities/Pedido";
-
-export interface IPedidoPresenter {
-    presenterPedidosParaRespostaHttp(message: string, success: boolean, pedidos: Pedido[]): any;
-    presenterPedidoParaRespostaHttp(message: string, success: boolean, pedido: Pedido): any;
-}
